f38258983cb6a2ba75f11be87dda3aa308c82a54
Sam Selikoff <sam.selikoff@gmail.com>
2019-03-17 22:07:42 -0700

    more

--- app/pods/solutions/8-inbox/component.js
+++ app/pods/solutions/8-inbox/component.js
@@ -1,11 +1,25 @@
 import Component from '@ember/component';
 import { A } from '@ember/array';
+import move from 'ember-animated/motions/move';
+import { fadeOut } from 'ember-animated/motions/opacity';
 
 export default Component.extend({
 
   /*
     Write your transition here
   */
+
+  * transition({ keptSprites, removedSprites, beacons }) {
+    removedSprites.forEach(sprite => {
+      sprite.endAtSprite(beacons.trash);
+      move(sprite);
+      fadeOut(sprite);
+    });
+
+    keptSprites.forEach(sprite => {
+      move(sprite);
+    });
+  },
 
   /*
     ____________________________
