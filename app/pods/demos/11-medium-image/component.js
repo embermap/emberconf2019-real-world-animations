97e3b15c780ee72c5d9d537f652f76fc877c4ea2
Sam Selikoff <sam.selikoff@gmail.com>
2019-03-17 22:57:45 -0700

    Comment out unused motion

--- app/pods/demos/11-medium-image/component.js
+++ app/pods/demos/11-medium-image/component.js
@@ -1,16 +1,17 @@
 import Component from '@ember/component';
-import { wait as waitUtil, Motion } from 'ember-animated';
 
 // Think of this motion as "freeze". The plan is to add it to ember-animated.
-function wait(sprite, opts) {
-  return new Wait(sprite, opts).run();
-}
+// import { wait as waitUtil, Motion } from 'ember-animated';
 
-class Wait extends Motion {
-  * animate() {
-    yield waitUtil(this.duration);
-  }
-}
+// function wait(sprite, opts) {
+//   return new Wait(sprite, opts).run();
+// }
+//
+// class Wait extends Motion {
+//   * animate() {
+//     yield waitUtil(this.duration);
+//   }
+// }
 
 export default Component.extend({
 
